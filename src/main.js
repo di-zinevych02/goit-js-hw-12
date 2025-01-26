c03524c7c9aaeebcb6340553287fac338e173bad
Diana Zinevych <dianazinevych02@gmail.com>
2025-01-26 20:11:25 +0100

    add true loader

--- src/main.js
+++ src/main.js
@@ -18,16 +18,15 @@ const loadMoreBtnEl = document.querySelector('.load-more-btn');
 let page = 1;
 let searchedQuery = '';
 let lightbox;
+const perPage = 15;
 //Приховаємо індикатор завантаження
 loader.style.display = 'none';
+loadMoreBtnEl.style.display = 'none';
 
 const onSearchFormSubmit = async event => {
-  try {
-    loader.style.display = 'block';
-    event.preventDefault();
-
+  loader.style.display = 'block';
+  event.preventDefault();
     searchedQuery = event.currentTarget.elements.user_query.value.trim();
-
     if (searchedQuery === "") {
       loader.style.display = 'none';
       iziToast.warning({
@@ -40,11 +39,11 @@ const onSearchFormSubmit = async event => {
     //Щоб потім новий запит при сабміті пішов за першою групою
     page = 1;
     //Приховуємо кнопку 
-    loadMoreBtnEl.classList.add('is-hidden');
-    const response = await fetchPhotosByQuery(searchedQuery, page);
-    loader.style.display = 'none';
-
-    if (response.data.hits.length === 0) {
+  loadMoreBtnEl.style.display = 'none';
+  
+    try {
+  const { data } = await fetchPhotosByQuery(searchedQuery, page);
+    if (data.hits.length === 0) {
       iziToast.error({
         message: 'Sorry, there are no images matching your search query. Please try again!',
         position: 'topRight',
@@ -58,15 +57,15 @@ const onSearchFormSubmit = async event => {
       return;
     }
     //Якщо при запиті зображень більше однієї групи то кнопка буде відображатись і прослуховуватиметься подія
-    if (response.data.totalHits > 1) {
-      loadMoreBtnEl.classList.remove('is-hidden');
+    if (data.totalHits > 1) {
+      loadMoreBtnEl.style.display = 'block';
       loadMoreBtnEl.addEventListener('click', onLoadMoreBtn);
     }
     
-    const galleryTemplate = response.data.hits.map(el => createGalleryCardTemplate(el)).join('');
+    const galleryTemplate = data.hits.map(el => createGalleryCardTemplate(el)).join('');
     galleryEl.innerHTML = galleryTemplate;
     // Показали користувачеві кнопку після відмалювання галереї
-    loadMoreBtnEl.classList.remove('is-hidden');
+    loadMoreBtnEl.style.display = 'block';
   
     lightbox = new SimpleLightbox('.gallery a', {
       captionsData: 'alt',
@@ -75,13 +74,14 @@ const onSearchFormSubmit = async event => {
     lightbox.refresh();
   
   } catch (err) {
-    loader.style.display = 'none';
     iziToast.error({
       title: 'Error',
       message: 'Something went wrong, please try again later',
       position: 'topRight',
-    })
+    }) 
     console.log(err);
+  } finally {
+    loader.style.display = 'none';
   }
 }
 
@@ -89,15 +89,30 @@ const onSearchFormSubmit = async event => {
 serchFormEl.addEventListener('submit', onSearchFormSubmit);
 
 const onLoadMoreBtn = async event => {
+  loader.style.display = 'block';
+  page++;
   try {
-    page++;
-    const response = await fetchPhotosByQuery(searchedQuery, page);
-    const galleryTemplate = response.data.hits.map(el => createGalleryCardTemplate(el)).join('');
+    const {data} = await fetchPhotosByQuery(searchedQuery, page);
+    loader.style.display = 'none';
+    const galleryTemplate = data.hits.map(el => createGalleryCardTemplate(el)).join('');
     //Додаємо після відмальованої картки наступну
     galleryEl.insertAdjacentHTML('beforeend', galleryTemplate);
     lightbox.refresh();
-  } catch (err) {
-    console.log(err);
-
+    //Якщо кількість карток буде більше або дорівнює загальної кількості карток на сервері то кнопку приховали 
+    if (page * perPage >= data.totalHits) {
+      
+      iziToast.info({
+        message: `Were sorry, but you've reached the end of search results.`,
+        position: 'topRight',
+      });
+      loadMoreBtnEl.style.display = 'none';
+    } 
+      
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      position: 'topRight',
+      message: 'Failed to load images. Please try again later.',
+    });
   }
 };
